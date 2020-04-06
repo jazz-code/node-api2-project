3dedf1214bdedb08f5ea700bbbac2c5fd7b2c597
ren.meta@gmail.com <ren.meta@gmail.com>
2020-04-06 16:19:40 -0400

    set up server, running

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+// const helmet = require("helmet");
+// const cors = require("cors");
+// const secrets = require("../config/secrets.js");
+const request = require('request')
+const server = express();
+
+server.use(express.json());
+// server.use(helmet());
+
+server.get("/", (req, res) => {
+    res.status(200).json(`Welcome to the Jungle`);
+  });
+
+module.exports = server;
